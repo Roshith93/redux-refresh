0e324bd65f25c008919b3e55477dd93fc3cd427f
Roshith R <r.roshith@gmail.com>
2020-11-25 08:36:34 +0530

    added basic counter with out redux

---
+++ src/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+ const [counterVal, setCounterVal] = useState(0)
+ const increment = () => {
+  setCounterVal(prevVal => prevVal + 1)
+ }
+ const decrement = () => {
+  setCounterVal(prevVal => prevVal - 1)
+ }
+ const reset = () => {
+  setCounterVal(0)
+ }
+
+ return (<div className="container">
+  <p className="counter">{counterVal}</p>
+  <div className="buttons">
+  <button type="button" onClick={increment} className="btn">increment</button>
+  <button onClick={reset} className="btn">reset</button>
+  <button onClick={decrement} className="btn">decrement</button>
+  </div>
+ </div>)
+}
+export default Counter
