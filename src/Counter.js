7a0e461d17a4d404619532e20c96f7bbca9f8ad3
Roshith R <r.roshith@gmail.com>
2020-11-26 10:40:48 +0530

    added dispatch actions to buttons

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {increment, decrement, reset} from './redux/actions/counter'
 
 const Counter = (props) => {
 
  return (<div className="container">
   <h3>{props.name}</h3>
   <p className="counter">{props.count}</p>
-  {/* <div className="buttons">
-  <button type="button" onClick={()=>inc} className="btn">increment</button>
-  <button onClick={reset} className="btn">reset</button>
-  <button onClick={dec} className="btn">decrement</button>
-  </div> */}
+  <div className="buttons">
+  <button type="button" onClick={props.increment} className="btn">increment</button>
+  <button onClick={props.reset} className="btn">reset</button>
+  <button onClick={props.decrement} className="btn">decrement</button>
+  </div>
  </div>)
 }
 const mapStateToProps = state => {
@@ -19,4 +20,11 @@ const mapStateToProps = state => {
   name: state.counter.name
  }
 }
-export default connect(mapStateToProps)(Counter)
+const mapDispatchToProps = dispatch => {
+ return {
+  increment: () => dispatch(increment()),
+  decrement: () => dispatch(decrement()),
+  reset: () => dispatch(reset()),
+ }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Counter)
