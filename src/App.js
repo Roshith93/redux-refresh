9fbf7db1c11181690c4321ec8e6f38ece34ffc7f
Roshith R <r.roshith@gmail.com>
2020-11-28 13:31:46 +0530

    added modal into app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Counter from './Counter'
+import Modal from './Modal'
 
 function App() {
   return (
     <div className="App">
       <Counter  />
+      <Modal/>
     </div>
   );
 }
