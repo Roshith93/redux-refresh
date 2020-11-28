cd178a3a83d817b6578e41c33d8ef8a3a171c8c7
Roshith R <r.roshith@gmail.com>
2020-11-28 13:30:04 +0530

    added modal to root reducer

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import counterReducer from './counter'
+import modalReducer from './modal'
 
 export const rootReducer = combineReducers({
-  counter:counterReducer
+  counter:counterReducer,
+  modal: modalReducer,
 })
