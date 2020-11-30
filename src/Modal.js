0128d18d4c001d5e6ac8e8356d3670a96a483102
Roshith R <r.roshith@gmail.com>
2020-11-30 06:24:34 +0530

    Added default props to the component

--- src/Modal.js
+++ src/Modal.js
@@ -1,20 +1,39 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
+import { closeModal } from './redux/actions/modal'
 
-const Modal = ({isOpen}) => {
- return(
-  <div className={`modal-overlay ${isOpen ? 'isModalOpen' : null}`}>
-   <div className="modal-container">
-    <h4>somerandom name</h4>
-    <p>some random text</p>
-    <button className="btn btn-primary">close</button>
-   </div>
-  </div>
- )
+const Modal = (props) => {
+  const { isOpen,title, text } = props
+  return (
+    <div className={`modal-overlay ${isOpen ? 'isModalOpen' : null}`}>
+      <div className='modal-container'>
+  <h4>{title}</h4>
+        <p>{text}</p>
+        <button
+          className='btn btn-primary'
+          onClick={() => props.closeModal(false)}
+        >
+          close
+        </button>
+      </div>
+    </div>
+  )
 }
-const mapStateToProps = state => {
- return{
-  isOpen: state.modal.isOpen
- }
+Modal.defaultProps = {
+  title: 'Welcome Modal',
+  text:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nihil voluptas cupiditate fuga vel non et dolor, neque, iure doloremque corporis quibusdam quam excepturi rem molestias dolorum voluptatum explicabo reiciendis.',
 }
-export default connect(mapStateToProps)(Modal);
+const mapStateToProps = ({ modal: { isOpen, title, text } }) => {
+  return {
+    isOpen,
+    title,
+    text,
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    closeModal: (val) => dispatch(closeModal(val)),
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Modal)
