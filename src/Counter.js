2565327299079f894fd728c31228c175a4ccec07
Roshith R <r.roshith@gmail.com>
2020-11-30 06:25:08 +0530

    dispatch two actions on reset

--- src/Counter.js
+++ src/Counter.js
@@ -1,30 +1,41 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {increment, decrement, reset} from './redux/actions/counter'
+import { connect } from 'react-redux'
+import { increment, decrement, reset } from './redux/actions/counter'
+import { openModal } from './redux/actions/modal'
 
 const Counter = (props) => {
-
- return (<div className="container">
-  <h3>{props.name}</h3>
-  <p className="counter">{props.count}</p>
-  <div className="buttons">
-  <button type="button" onClick={props.increment} className="btn">increment</button>
-  <button onClick={props.reset} className="btn">reset</button>
-  <button onClick={props.decrement} className="btn">decrement</button>
-  </div>
- </div>)
+  return (
+    <div className='container'>
+      <h3>{props.name}</h3>
+      <p className='counter'>{props.count}</p>
+      <div className='buttons'>
+        <button type='button' onClick={props.increment} className='btn'>
+          increment
+        </button>
+        <button onClick={() => props.reset(true)} className='btn'>
+          reset
+        </button>
+        <button onClick={props.decrement} className='btn'>
+          decrement
+        </button>
+      </div>
+    </div>
+  )
 }
-const mapStateToProps = state => {
- return{
-  count: state.counter.count,
-  name: state.counter.name
- }
+const mapStateToProps = ({ counter: { count, name } }) => {
+  return {
+    count,
+    name,
+  }
 }
-const mapDispatchToProps = dispatch => {
- return {
-  increment: () => dispatch(increment()),
-  decrement: () => dispatch(decrement()),
-  reset: () => dispatch(reset()),
- }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => dispatch(increment()),
+    decrement: () => dispatch(decrement()),
+    reset: (val) => {
+      dispatch(openModal(val))
+      dispatch(reset())
+    },
+  }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Counter)
